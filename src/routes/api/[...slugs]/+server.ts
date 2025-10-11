e0e6e6385b7997424b5ef183401d5bb78b86944f
FForFachriza <dimasavrian346@gmail.com>
2025-10-11 21:28:02 +0700

    feat(backend) : add middleware to check if user logged in or nah

--- src/routes/api/[...slugs]/+server.ts
+++ src/routes/api/[...slugs]/+server.ts
@@ -1,9 +1,27 @@
 import { Elysia } from "elysia";
 import { swagger } from "@elysiajs/swagger";
 import { PUBLIC_API_PATH } from "$env/static/public";
+import { auth } from "@/server/auth";
 
 const app = new Elysia({ prefix: PUBLIC_API_PATH })
   .use(swagger())
+  .mount(auth.handler)
+  .macro({
+    auth: {
+      async resolve({ status, request: { headers } }) {
+        const session = await auth.api.getSession({
+          headers,
+        });
+
+        if (!session) return status(401);
+
+        return {
+          user: session.user,
+          session: session.session,
+        };
+      },
+    },
+  })
   .get("/hi", () => "Hi Elysia");
 
 export type App = typeof app;
